6a4278b2e690e2137bc107266bf71da0ba11195f
Yasin Onur Yücel <yoy1992@gmail.com>
2024-12-22 16:47:42 +0100

    Refactor: "Optimized useMemo dependencies in AppWalletProvider by removing unnecessary dependencies."

--- src/app/appWalletProvider.tsx
+++ src/app/appWalletProvider.tsx
@@ -1,33 +1,29 @@
-"use client"
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import {
-    WalletModalProvider
-} from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
-import { useMemo } from 'react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+"use client";
+
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 
 require("@solana/wallet-adapter-react-ui/styles.css");
 
- 
 export default function AppWalletProvider({
-    children,
-  }: {
+                                              children,
+                                          }: {
     children: React.ReactNode;
-  }) {
+}) {
     const network = WalletAdapterNetwork.Devnet;
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-    const wallets = useMemo(
-      () => [
-      ],
-      [network],
-    );
-   
+
+    // Removed 'network' from dependency arrays because it's a constant
+    const endpoint = useMemo(() => clusterApiUrl(network), []);
+    const wallets = useMemo(() => [], []);
+
     return (
-      <ConnectionProvider endpoint={endpoint}>
-        <WalletProvider wallets={wallets} autoConnect>
-          <WalletModalProvider>{children}</WalletModalProvider>
-        </WalletProvider>
-      </ConnectionProvider>
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>{children}</WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
     );
-  }
+}
